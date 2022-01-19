9962849bbb2a94ad2f2a385e38fd02df76705cfc
Daniel Mosso <dmosso@dhd.com>
2022-01-19 13:10:55 -0800

    FindSum

---
+++ test.js
@@ -0,0 +1,13 @@
+function Hola (n) { 
+
+let sum = 0;
+let result = [];
+for (var i = 0; i <= n; i++) {
+    if (i === 5 || i === (2+2)) {
+        result.push(i)
+    }
+}
+console.log(result)
+}
+
+Hola(10)
